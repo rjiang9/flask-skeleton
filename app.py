3d86ed11f1fd0dc1e20b374b0dce3296caa419a1
Ray Jiang <rjiang9@users.noreply.github.com>
2023-06-23 14:21:18 -0600

    clean up code

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from Cywriter.models import Spec
 from Cywriter.category.models import Category
 from Cywriter.selector.models import Selector
 from Cywriter.assertion.models import Assertion
-from Cywriter.it.models import It
+from Cywriter.step.models import Step
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Spec': Spec, 'Category': Category, 'Selector': Selector, 'Assertion': Assertion} # include sub apps
+    return {'db': db, 'User': User, 'Spec': Spec, 'Category': Category, 'Step': Step, 'Selector': Selector, 'Assertion': Assertion} # include sub apps
 
